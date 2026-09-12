56ebea7bd67400e016ca122424afd25ec096d165
prayagchakraborty2006 <prantarprayagchakraborty@gmail.com>
2026-09-12 20:51:44 +0530

    Fix model path case sensitivity
    Updated model path to match filename case sensitivity.

--- Module 3/Image_Classification/app.py
+++ Module 3/Image_Classification/app.py
@@ -8,13 +8,14 @@ import uvicorn
 from pathlib import Path
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+
 print("Current folder:", BASE_DIR)
 print("Files here:", os.listdir(BASE_DIR))
-# Correct model path
-BASE_DIR = Path(__file__).resolve().parent
-MODEL_PATH = BASE_DIR / "cyclone_resnet18_best.pth"
 
-# Load model ONCE
+MODEL_PATH = BASE_DIR / "Cyclone_resnet18_best.pth"
+
 model, class_names, device = load_trained_model(str(MODEL_PATH))
 
 @app.post("/predict")
